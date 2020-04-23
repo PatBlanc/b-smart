6be00a5ca6caa92636438304e40c7904181d5af6
Patrick Blanc <perso@patrickblanc.net>
2020-04-23 18:52:31 +0200

    schema dependencies... another try

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_102030) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vehicules_on_user_id"
   end
 
 end
